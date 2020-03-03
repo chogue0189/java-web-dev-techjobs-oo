e12cc87f5a7fb1091e42ca34af1e9d9b5c97e88d
cameron <chogue0189@gmail.com>
2020-03-02 20:04:33 -0600

    Test jobs for equality

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -11,14 +11,17 @@ public class JobTest {
     private static Job job2;
     private static Job testJob1;
     private static Job testJob2;
-    private
+    private static Job testJob3;
+
 
     //Test constructor is setting a unique ID for each job
     @Test
     public void testSettingJobId() {
-        job1 = new Job();
-        job2 = new Job();
-
+        job1 = new Job ();
+        job2 = new Job ();
+        testJob1 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        testJob2 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality Control"), new CoreCompetency("Persistence"));
+        testJob3 = new Job("", new Employer(""), new Location(""), new PositionType(""), new CoreCompetency(""));
         assertEquals(1, job2.getId() - job1.getId(), .001);
         //assertFalse(job1.equals(job2));
         //assertTrue((job1.getId()) < (job2.getId()));
