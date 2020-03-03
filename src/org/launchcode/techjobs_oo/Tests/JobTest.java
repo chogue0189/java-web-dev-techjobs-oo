c5f72c2d474e7ebcbadfd0ce3e20aba03efc78dc
cameron <chogue0189@gmail.com>
2020-03-02 19:45:31 -0600

    Test jobs for equality

--- src/org/launchcode/techjobs_oo/Tests/JobTest.java
+++ src/org/launchcode/techjobs_oo/Tests/JobTest.java
@@ -10,27 +10,33 @@ public class JobTest {
     private static Job job1;
     private static Job job2;
     private static Job testJob1;
+    private static Job testJob2;
+    private
 
+    //Test constructor is setting a unique ID for each job
     @Test
     public void testSettingJobId() {
         job1 = new Job();
         job2 = new Job();
 
-        assertEquals(job1, job2);
+        assertEquals(1, job2.getId() - job1.getId(), .001);
         //assertFalse(job1.equals(job2));
         //assertTrue((job1.getId()) < (job2.getId()));
     }
 
+    //Test constructor is assigning class and value of each field
     @Test
     public void testJobConstructorSetsAllFields() {
         testJob1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
                 new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        testJob2 = new Job("Product tester", new Employer("ACME"), new Location("Desert"),
+                new PositionType("Quality control"), new CoreCompetency("Persistence"));
         assertEquals(testJob1.getEmployer(), testJob1.getValue(), testJob1.getLocation(), testJob1.getValue());
 
     }
-
+    //Test jobs for equality
     @Test
     public void testJobsForEquality(){
-
+        assertFalse(testJob1.equals(testJob2));
     }
 }
